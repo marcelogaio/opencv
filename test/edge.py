dd4bd395befbb7f5a9bef2bcb3b1063dd5044ab9
marcelo.gaio@groovinads.com <marcelo.gaio@groovinads.com>
2018-12-16 19:37:30 +0100

    updates

--- test/edge.py
+++ test/edge.py
@@ -3,7 +3,7 @@ import cv2 as cv
 scale = 1
 delta = 0
 ddepth = cv.CV_16S
-
+canny_threshold = 10
 
 def preProcess(frame):
     # Convert image to grayscale
@@ -15,25 +15,54 @@ def preProcess(frame):
 
 def sketchCanny(frame):
     # Extract Edges
-    canny = cv.Canny(frame, 10, 70)
-    # Do an invert binarize the image
-    ret, frame = cv.threshold(canny, 70, 255, cv.THRESH_BINARY)
+    frame = cv.Canny(frame, canny_threshold, canny_threshold*3, 3)
     return frame
 
 
-def showWindow(title, frame, x, y):
-    fps = round(cv.getTickFrequency() / (cv.getTickCount() - start))
+def sketchInvert(frame):
+    # Do an invert binarize the image
+    _, frame = cv.threshold(frame, 20, 255, cv.THRESH_BINARY)
+    return frame
+
+
+def on_trackbar_canny(val):
+    canny_threshold = val
+    print(val)
+
+
+def createWindow(title, x, y, trackbar_array=()):
+    # create window
     cv.namedWindow(title, cv.WINDOW_NORMAL)
+    # create trackbars
+    for values in trackbar_array:
+        trackbar_name = values[0]
+        trackbar_min_val = values[1]
+        trackbar_max_val = values[2]
+        trackbar_current_val = values[3]
+        trackbar_function = values[4]
+        trackbar_name = trackbar_name + ' x %d' % trackbar_max_val
+        cv.createTrackbar(trackbar_name, title, trackbar_current_val, trackbar_max_val, trackbar_function)
+        cv.setTrackbarMin(trackbar_name, title, trackbar_min_val)
     cv.resizeWindow(title, 800, 500)
+    cv.moveWindow(title, x, y)
+
+
+def updateImage(title, frame):
+    fps = round(cv.getTickFrequency() / (cv.getTickCount() - start))
     cv.putText(frame, str(fps), (0, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
     cv.imshow(title, frame)
-    cv.moveWindow(title, x, y)
 
 
 # init video capture
 capture = cv.VideoCapture(0)
 capture.set(cv.CAP_PROP_FPS, 1)
 
+# declare trackbars
+trackbar_canny = [['Canny Threshold', 0, 100, canny_threshold, on_trackbar_canny]]
+
+# create windows
+createWindow("Canny", 20, 20, trackbar_canny)
+createWindow("Canny + Invert", 820, 20)
 
 # loop frames
 while True:
@@ -41,9 +70,11 @@ while True:
     _, frame = capture.read()
     if frame is not None:
         frame = preProcess(frame)
-        showWindow("Frame", frame, 20, 20)
-        showWindow("Canny", sketchCanny(frame), 820, 20)
+        # update images
+        updateImage("Canny", sketchCanny(frame))
+        updateImage("Canny + Invert", sketchCanny(sketchInvert(frame)))
 
+    # capture esc key
     key = cv.waitKey(1)
     if key == 27:
         break
--- test/face_recognition.py
+++ test/face_recognition.py
@@ -1,13 +1,8 @@
-from __future__ import print_function
 import cv2 as cv
 
-capture = cv.VideoCapture(0)
-algo = 'MOG2' ''' MOG2 or KNN '''
-
-if algo == 'MOG2':
-    backSub = cv.createBackgroundSubtractorMOG2()
-else:
-    backSub = cv.createBackgroundSubtractorKNN()
+scale = 1
+delta = 0
+ddepth = cv.CV_16S
 
 
 def preProcess(frame):
@@ -15,6 +10,14 @@ def preProcess(frame):
     frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # Cleanup image using gaussian blur
     frame = cv.GaussianBlur(frame, (5, 5), 0)
+    return frame
+
+
+def sketchCanny(frame):
+    # Extract Edges
+    canny = cv.Canny(frame, 10, 70)
+    # Do an invert binarize the image
+    ret, frame = cv.threshold(canny, 70, 255, cv.THRESH_BINARY)
     return frame
 
 
@@ -27,27 +30,25 @@ def showWindow(title, frame, x, y):
     cv.moveWindow(title, x, y)
 
 
+# init video capture
+capture = cv.VideoCapture(0)
+capture.set(cv.CAP_PROP_FPS, 1)
+
+
+# loop frames
 while True:
     start = cv.getTickCount()
     _, frame = capture.read()
     if frame is not None:
         frame = preProcess(frame)
-
-        frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        frame_blurred = cv.GaussianBlur(frame_gray, (5, 5), 0)
-        frame_canny = cv.Canny(frame_blurred, 50, 150)
-
-        fgMask = backSub.apply(frame)
-
-        cv.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
-        cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-
-        showWindow("Canny", frame_canny, 20, 20)
-        showWindow("FG Mask", fgMask, 820, 20)
+        showWindow("Frame", frame, 20, 20)
+        showWindow("Canny", sketchCanny(frame), 820, 20)
 
     key = cv.waitKey(1)
     if key == 27:
         break
 
+# release video capture
 capture.release()
+# destroy windows
 cv.destroyAllWindows()
